ecf4f946ef016259b434dcce3ded75c4e8c1adb5
Carter Appleton <carter.appleton@gmail.com>
2015-04-09 12:44:54 -0400

    Initial Commit

---
+++ Messenger/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Messenger
+//
+//  Created by Carter Appleton on 4/9/15.
+//  Copyright (c) 2015 Carter Appleton. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
